6d01db0df5a2f4af6097eb2d491bc67a33a5ae3d
Mukul Sharma <mukulsharma262002@gmail.com>
2023-08-13 17:26:14 +0530

    Fixed the start game bug.

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function getRandomCard (){
 
 startGame = () => {
     isAlive = true
+    hasBlackJack = false
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards  = [firstCard, secondCard]
